4976cd4f5fc00101b5092d7455e33b084649e845
richell <richell.bonfield12@gmail.com>
2020-04-19 09:43:29 -0500

    Added songs models and migrations

---
+++ ovos-library-api/db/migrate/20200414112101_create_songs.rb
@@ -0,0 +1,13 @@
+class CreateSongs < ActiveRecord::Migration[6.0]
+    def change
+      create_table :songs do |t|
+        t.string :title
+        t.string :album
+        t.string :release_year
+        t.string :artist_id
+  
+        t.timestamps
+  
+      end
+    end
+  end
